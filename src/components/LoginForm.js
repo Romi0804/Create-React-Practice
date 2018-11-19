19a9807959840855e9b1e3dcb9e1889e8dce49fb
Ham Choromi <choromi0804@gmail.com>
2018-11-19 17:06:04 +0900

    Ref 사용

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,10 +2,20 @@ import React from 'react'
 import api from '../api';
 
 export default class LoginForm extends React.Component {
+    constructor(props) {
+      super(props)
+    
+     this.usernameRef = React.createRef()
+     this.passwordRef = React.createRef()
+    }
+    
     async handleSubmit(e){
         e.preventDefault()
-        const username = e.target.elements.username.value
+        const username = this.usernameRef.current.value
         const password = e.target.elements.password.value
+        //Ref.current와 e.target.elements는 같다. 비교할겸 남겨두는 것
+        //form을 안쓰고 제어되지 않는 컴포넌트를 쓰고 싶다 그렇다면 DOM 객체를 가지고 올 방법이 필요해
+        //그럴때 Ref를 써야해. 그럴때는 Ref밖에 방법이 없거든 
         const res = await api.post ('/users/login', {
             username,
             password
@@ -19,8 +29,8 @@ export default class LoginForm extends React.Component {
             <React.Fragment>
             <form onSubmit={e => this.handleSubmit(e)}>
                 <h1>로그인</h1>
-                <input type="text" name="username" />
-                <input type="password" name="password" />
+                <input ref={this.usernameRef} type="text" name="username" />
+                <input ref={this.passwordRef} type="password" name="password" />
                 <button>로그인</button>
             </form>
             <button onClick={() => onRegister()}>회원가입</button>
